9f2bbdb44d5aebe05a277d046a1a260c82fdb71a
Kayna2D <unifkaysilva@fei.edu.br>
2024-10-12 07:14:30 -0300

    conexao com bd

---
+++ ExchangeCripto/src/DAO/InvestidorDAO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Investidor;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Usuario
+ */
+public class InvestidorDAO {
+    private Connection conn;
+
+    public InvestidorDAO(Connection conn) {
+        this.conn = conn;
+    }
+    
+    public ResultSet logar(Investidor investidor) throws SQLException {
+        String sql = "select * from investidor where cpf = '"
+                + investidor.getCpf() + "' AND senha = '"
+                + investidor.getSenha() + "'";
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.execute();
+        ResultSet resultado = statement.getResultSet();
+        return resultado;
+    }
+}
