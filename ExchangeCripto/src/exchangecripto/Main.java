8b1ecb52fdcca4228ff7f0c4cc419bc08c3e2a48
Kayna2D <unifkaysilva@fei.edu.br>
2024-10-12 07:06:02 -0300

    janela login

---
+++ ExchangeCripto/src/exchangecripto/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exchangecripto;
+
+import view.LoginFrame;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+    public static void main(String[] args) {
+        LoginFrame lf = new LoginFrame();
+        
+        lf.setVisible(true);
+    }
+}
